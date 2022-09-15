3df83b8a50ce95f6f49dc9811ef3ac9b7ee3efab
lucasnevespereira <pereiraneveslucas@gmail.com>
2022-09-15 10:35:50 +0100

    add page title

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,15 @@ import { CartProvider } from "../context/cart";
 import { ChakraProvider } from "@chakra-ui/react";
 import Cart from "../components/Cart/Cart";
 import Footer from "../components/Footer/Footer";
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <CartProvider>
       <ChakraProvider>
+        <Head>
+        <title>Vyreca Creations</title>
+        </Head>
         <Cart />
         <Component {...pageProps} />
         <Footer />
